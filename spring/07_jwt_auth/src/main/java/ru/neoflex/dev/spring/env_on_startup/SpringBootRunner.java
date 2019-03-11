ae4c322d45cfe0d359fbed0aaa63ed4953a8e97a
pavel <pbuhmatov@neoflex.ru>
2019-03-11 16:11:29 +0300

    Add more text to 7

--- spring/07_jwt_auth/src/main/java/ru/neoflex/dev/spring/env_on_startup/SpringBootRunner.java
+++ spring/07_jwt_auth/src/main/java/ru/neoflex/dev/spring/env_on_startup/SpringBootRunner.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SpringBootRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRunner.class, args);
