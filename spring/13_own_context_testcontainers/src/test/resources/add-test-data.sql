5b80a6c1fa21bcf106464a057fe61ce5640348b4
pavel <pbuhmatov@neoflex.ru>
2019-03-22 10:41:59 +0300

    Initial configuration for 13

---
+++ spring/13_own_context_testcontainers/src/test/resources/add-test-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO DEPARTMENT(ID, NAME) values
+  (1, 'FRIR'),
+  (2, 'NFO');
+
+INSERT INTO EMPLOYEE(ID, NAME, AGE, DEPARTMENT_ID) values
+  (1, 'John', 18, 1),
+  (2, 'Mark', 25, 1),
+  (3, 'Samantha', 19, 2),
+  (4, 'Kate', 38, 2);
